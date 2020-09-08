9398157c98b38f2076bee56772b110ef6e245a5a
Aarush Gupta <65785561+et-sollertis-animi@users.noreply.github.com>
2020-09-08 15:29:42 -0700

    Added Mining Ability
    - Can mine coin through CoinTest.py without errors
    - Adds 50 AarushCoin as Miner Rewards
    - Need to implement balances and keys (RSA)

--- AarushCoin.py
+++ AarushCoin.py
@@ -1,11 +1,35 @@
 from time import time
+from datetime import datetime
 import json
 import hashlib
 import random
 
 class Blockchain(object):
 	def __init__(self):
-		self.chain = []
+		self.chain = [self.addGenesisBlock()]
+		self.pendingTransactions = []
+		self.difficulty = 3
+		self.minerRewards = 50
+		self.blockSize = 10
+
+	def generateKeys(self):
+		pass
+	
+	def minePendingTransactions(self, miner):
+		for i in range(0, len(self.pendingTransactions), self.blockSize):
+			end = i + self.blockSize
+			if i >= len(self.pendingTransactions):
+				end = len(self.pendingTransactions)
+			transactionSlice = self.pendingTransactions[i:end]
+			newBlock = Block(transactionSlice, datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), len(self.chain))
+			hashVal = self.getLastBlock().hash
+			newBlock.previousBlockHash = hashVal
+			newBlock.mineBlock(self.difficulty)
+			self.chain.append(newBlock)
+			print("Mining Transactions Success!")
+			payMiner = Transaction("Miner Rewards", miner, self.minerRewards)
+			self.pendingTransactions = [payMiner]
+		return True
 
 	def addBlock(self, block):
 		if len(self.chain) > 0:
@@ -27,7 +51,24 @@ class Blockchain(object):
 			innerOutput["time"] = block.time
 			innerOutput["nonce"] = block.nonce
 			output.append(innerOutput)
+			transactionOutput = []
+			transactionInnerOutput = {}
+			for transaction in block.transactions:
+				transactionInnerOutput["time"] = transaction.time
+				transactionInnerOutput["sender"] = transaction.sender
+				transactionInnerOutput["reciever"] = transaction.reciever
+				transactionInnerOutput["amount"] = transaction.amount
+				transactionInnerOutput["hash"] = transaction.hash
+				transactionOutput.append(transactionInnerOutput)
+			innerOutput["transactions"] = transactionOutput
 		return output
+
+	def addGenesisBlock(self):
+		transactionArray = []
+		transactionArray.append(Transaction("Arjun", "Aarush", 10))
+		genesis = Block(transactionArray, datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), 0)
+		genesis.previousBlockHash = "None"
+		return genesis
 
 class Block(object):
 	def __init__(self, transactions, time, index):
@@ -37,6 +78,21 @@ class Block(object):
 		self.previousBlockHash = ""
 		self.nonce = 0
 		self.hash = self.calculateHash()
+
+	def mineBlock(self, difficulty):
+		array = []
+		for i in range(0, difficulty):
+			array.append(i)
+		arrayString = map(str, array)
+		hashPuzzle = "".join(arrayString)
+		while self.hash[0:difficulty] != hashPuzzle:
+			self.nonce += 1
+			self.hash = self.calculateHash()
+			print(f"Nonce: {self.nonce}")
+			print(f"Hash Attempt {self.hash}")
+		print(f"Hash We Want {hashPuzzle} ... \n")
+		print(f"Block Mined! Nonce to Solve Proof of Work: {self.nonce}")
+		return True
 
 	def calculateHash(self):
 		hashTransactions = ""
@@ -54,7 +110,7 @@ class Transaction(object):
 		self.time = time()
 		self.hash = self.calculateHash()
 
-		def calculateHash(self):
-			hashString = self.sender + self.reciever + str(self.amount) + str(self.time)
-			hashEncoded = json.dumps(hashString, sortKeys=True).encode()
-			return hashlib.sha256(hashEncoded).hexdigest()
+	def calculateHash(self):
+		hashString = self.sender + self.reciever + str(self.amount) + str(self.time)
+		hashEncoded = json.dumps(hashString).encode()
+		return hashlib.sha256(hashEncoded).hexdigest()
--- CoinTest.py
+++ CoinTest.py
@@ -5,6 +5,26 @@ import pprint
 prettyPrint = pprint.PrettyPrinter(indent=4)
 
 blockchain = Blockchain()
+
+transaction = Transaction("Person1", "Person2", 10)
+blockchain.pendingTransactions.append(transaction)
+
+transaction = Transaction("Person1", "Person2", 10)
+blockchain.pendingTransactions.append(transaction)
+
+blockchain.minePendingTransactions("Aarush")
+blockchain.minePendingTransactions("Arjun")
+
+prettyPrint.pprint(blockchain.packageChain())
+print(f"Length: {len(blockchain.chain)}")
+"""
+from AarushCoin import *
+from time import time
+import pprint
+
+prettyPrint = pprint.PrettyPrinter(indent=4)
+
+blockchain = Blockchain()
 transactions = []
 
 block = Block(transactions, time(), 0)
@@ -18,3 +38,4 @@ blockchain.addBlock(block)
 
 prettyPrint.pprint(blockchain.packageChain())
 print(f"Length: {len(blockchain.chain)}")
+"""
